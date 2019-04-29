df1b56e4a2fa2df2787da3028ac3eda716c8a6d7
UnicornWizard375 <drumm040@morris.umn.edu>
2019-04-29 12:55:53 -0500

    added chat files, however they do not work yet. I'm just pushing to save my work

---
+++ client/src/app/chat/chat.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+//import {RideListService} from './ride-list.service';
+import {Observable} from 'rxjs/Observable';
+import {MatDialog} from "@angular/material";
+
+@Component({
+  selector: 'chat-component',
+  templateUrl: 'chat.component.html',
+  styleUrls: ['./chat.component.scss'],
+  providers: []
+})
+
+export class ChatComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit(){
+
+  }
+
+}

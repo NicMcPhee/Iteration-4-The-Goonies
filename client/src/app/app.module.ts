4dc0522c86e5429664a283d33ab9738b101fc5c9
carri320 <carri320@morris.umn.edu>
2019-04-30 14:46:52 -0500

    Fixed the loading issue and Chatcomponent declaration

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -32,6 +32,7 @@ import {ProfileComponent} from "./users/profile.component";
 
 import {RouterLinkDirectiveStub} from "./rides/router-link-directive-stub";
 import {PhoneMaskDirective} from "./users/phone-mask.directive";
+import {ChatComponent} from "./chat/chat.component";
 
 @NgModule({
   imports: [
@@ -54,7 +55,8 @@ import {PhoneMaskDirective} from "./users/phone-mask.directive";
     DeleteRideComponent,
     ProfileComponent,
     RouterLinkDirectiveStub,
-    PhoneMaskDirective
+    PhoneMaskDirective,
+    ChatComponent
   ],
   providers: [
     RideListService, AppService, AppAuthGuard, UserService, ValidatorService,

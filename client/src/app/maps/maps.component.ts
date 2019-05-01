901016a54236069a99ff3a6cf883efad8b218cbb
xion1547 <xion1547@morris.umn.edu>
2019-04-30 21:47:54 -0500

    Adds inputs and imports to maps.component.ts to allow variables

--- client/src/app/maps/maps.component.ts
+++ client/src/app/maps/maps.component.ts
@@ -1,3 +1,9 @@
+import {Component, Input, OnInit} from "@angular/core";
+import {AgmMap} from "@agm/core";
+import LatLngBounds = google.maps.LatLngBounds
+import {} from 'googlemaps';
+
+
 @Component({
   selector: 'maps-component',
   templateUrl: './maps.component.html',
@@ -5,5 +11,9 @@
 })
 
 export class MapsComponent {
+  @Input() mapHeight: string = "30";
+  @Input() lat: number = 45.593614;
+  @Input() lng: number = -95.890831;
+  @Input() zoom: number = 12;
 
 }

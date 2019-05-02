f6a97f952a33cc48b317957809ce0ef95305aec2
xion1547 <xion1547@morris.umn.edu>
2019-05-02 13:13:12 -0500

    Adds ngOnInit to maps component for maps to start with an initial height. -With @wegle030

--- client/src/app/maps/maps.component.ts
+++ client/src/app/maps/maps.component.ts
@@ -10,10 +10,14 @@ import {} from 'googlemaps';
   styleUrls: ['./maps.component.css']
 })
 
-export class MapsComponent {
+export class MapsComponent implements OnInit{
   @Input() mapHeight: string = "30";
   @Input() lat: number = 45.593614;
   @Input() lng: number = -95.890831;
   @Input() zoom: number = 12;
+
+  ngOnInit() {
+    document.getElementById("maps").style.height = this.mapHeight + "vh";
+  }
 
 }

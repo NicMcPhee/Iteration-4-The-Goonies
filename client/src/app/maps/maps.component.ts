e5db0aeb3e278347ada27336b7a4b2d4b3078f77
xion1547 <xion1547@morris.umn.edu>
2019-05-02 13:18:53 -0500

    Imports marker to maps.component.ts and adds an array of markers -With @wegle030

--- client/src/app/maps/maps.component.ts
+++ client/src/app/maps/maps.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from "@angular/core";
 import {AgmMap} from "@agm/core";
 import LatLngBounds = google.maps.LatLngBounds
 import {} from 'googlemaps';
+import {Marker} from "./marker";
 
 
 @Component({
@@ -15,9 +16,12 @@ export class MapsComponent implements OnInit{
   @Input() lat: number = 45.593614;
   @Input() lng: number = -95.890831;
   @Input() zoom: number = 12;
+  @Input() markers: Marker[] = [];
 
   ngOnInit() {
     document.getElementById("maps").style.height = this.mapHeight + "vh";
   }
+
+  
 
 }

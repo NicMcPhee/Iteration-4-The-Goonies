f4607f3e5b30eda5089816daa7553f64687e5a2f
ColtDahl <dahlcolt@gmail.com>
2019-05-02 13:06:40 -0500

    Add roundtrip/nonsmoking support to claimride

--- client/src/app/rides/claim-ride.component.ts
+++ client/src/app/rides/claim-ride.component.ts
@@ -120,7 +120,7 @@ export class ClaimRideComponent implements OnInit {
     this.rideDepartureTime = this.rideListService.singleRide.departureTime;
     this.rideNonSmoking = this.rideListService.singleRide.nonSmoking;
     this.rideRoundTrip = this.rideListService.singleRide.roundTrip;
-    
+
       this.rideNotes = "This ride created for " + this.rideListService.singleRide.user + ".";
   }
 

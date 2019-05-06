56f662d71dedaaacbdd6503217a290bd01509ad5
UnicornWizard375 <drumm040@morris.umn.edu>
2019-05-05 21:10:36 -0500

    Work on server side chat database implementation #16
    Co-Authored-by: Laly <xion1337@morris.umn.edu>

--- server/src/main/java/umm3601/chat/ChatController.java
+++ server/src/main/java/umm3601/chat/ChatController.java
@@ -1,4 +1,63 @@
 package umm3601.chat;
 
+//Code taken/inspired from Newton's Fig Iteration 3:
+// https://github.com/UMM-CSci-3601-S19/iteration-3-newton-s-fig/blob/master/server/src/main/java/umm3601/chat/ChatController.java
+
+
+import com.mongodb.MongoException;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.util.Iterator;
+
+import static com.mongodb.client.model.Filters.eq;
+
 public class ChatController {
+
+  private final MongoCollection<Document> chatCollection;
+
+  public ChatController(MongoDatabase database) {
+    chatCollection = database.getCollection("chats");
+  }
+
+  public String getChat(String id) {
+    FindIterable<Document> jsonChats
+      = chatCollection
+      .find(eq("rideID", new ObjectId(id)));
+
+    Iterator<Document> iterator = jsonChats.iterator();
+    if (iterator.hasNext()) {
+      Document chat = iterator.next();
+      return chat.toJson();
+    } else {
+      return null;
+    }
+  }
+
+  public String getMessages(String id) {
+    Document chat = Document.parse(getChat(id));
+    return chat.get("messages").toString();
+  }
+
+  public String addNewChat(String rideID) {
+
+    Document newChat = new Document();
+    newChat.append("rideID", rideID);
+    //newChat.append("chatArray", arrayofmessage);
+
+    try {
+      chatCollection.insertOne(newChat);
+      ObjectId id = newChat.getObjectId("rideID");
+      return id.toHexString();
+    }
+    catch (MongoException me) {
+      me.printStackTrace();
+      return null;
+    }
+  }
+
+
 }
--- server/src/main/java/umm3601/chat/ChatRequestHandler.java
+++ server/src/main/java/umm3601/chat/ChatRequestHandler.java
@@ -1,4 +1,60 @@
 package umm3601.chat;
 
+//Code taken/inspired from Newton's Fig Iteration 3:
+// https://github.com/UMM-CSci-3601-S19/iteration-3-newton-s-fig/blob/master/server/src/main/java/umm3601/chat/ChatController.java
+
+
+
+
+//to get these imports to work you may have to run: "npm install getstream" and "npm install jsonwebtoken" in the intelij command line
+
+import org.bson.Document;
+import spark.Request;
+import spark.Response;
+
 public class ChatRequestHandler {
+
+  public final ChatController chatController;
+
+  public ChatRequestHandler(ChatController chatController) {
+    this.chatController = chatController;
+  }
+
+  public String getChatJSON(Request req, Response res) {
+    res.type("application/json");
+    String chatID = req.params("id");
+    System.err.println("The chat chatId req params got is " + chatID);
+    String chat;
+    try {
+      chat = chatController.getChat(chatID);
+    } catch (IllegalArgumentException e) {
+      // This is thrown if the ID doesn't have the appropriate
+      // form for a Mongo Object ID.
+      // https://docs.mongodb.com/manual/reference/method/ObjectId/
+      res.status(400);
+      res.body("The requested user chatID " + chatID + " wasn't a legal Mongo Object ID.\n" +
+        "See 'https://docs.mongodb.com/manual/reference/method/ObjectId/' for more info.");
+      return "";
+    }
+    if (chat != null) {
+      return chat;
+    } else {
+      res.status(404);
+      res.body("The requested user with userId " + chatID + " was not found");
+      return "";
+    }
+  }
+
+  public String addNewChat(Request req, Response res) {
+    res.type("application/json");
+
+    Document newChat = Document.parse(req.body());
+
+    String rideID = newChat.getString("RideID");
+    //Array chatArray = newChat.parse("chatArray");
+
+    return chatController.addNewChat(rideID);
+
+  }
+
 }

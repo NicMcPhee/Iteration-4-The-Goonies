fa790f4fa9f795701167d970d5f74917e33f3a5c
xion1547 <xion1547@morris.umn.edu>
2019-05-08 23:33:35 -0500

    Fixes height of map and adds the filledMarkers function

--- client/src/app/maps/maps.component.ts
+++ client/src/app/maps/maps.component.ts
@@ -10,11 +10,38 @@ import {Marker} from "./marker";
 })
 
 export class MapsComponent implements OnInit{
-  @Input() mapHeightVH: string = "30";
+  @Input() mapHeightVH: string = "100";
   @Input() lat: number = 45.593614;
   @Input() lng: number = -95.890831;
   @Input() zoom: number = 12;
   @Input() markers: Marker[] = [];
+
+  filledMarkers(): Marker[] {
+    return this.markers.filter(x => (x['longitude'] && x['latitude']));
+  }
+  //
+  // setRideOrigin(placeResult: google.maps.places.PlaceResult) {
+  //   console.log(placeResult);
+  //   this.rideOrigin = placeResult;
+  //   let m: Marker = {
+  //     longitude: placeResult.geometry.location.lng(),
+  //     latitude: placeResult.geometry.location.lat(),
+  //     label: 'A'
+  //   };
+  //   this.markers[0] = m;
+  //   this.markers = this.markers.slice();
+  // }
+  //
+  // setRideDestination(placeResult: google.maps.places.PlaceResult) {
+  //   this.rideDestination = placeResult;
+  //   let m: Marker = {
+  //     longitude: placeResult.geometry.location.lng(),
+  //     latitude: placeResult.geometry.location.lat(),
+  //     label: 'B'
+  //   };
+  //   this.markers[1] = m;
+  //   this.markers = this.markers.slice();
+  // }
 
   ngOnInit() {
     document.getElementById("map").style.height = this.mapHeightVH + "vh";
